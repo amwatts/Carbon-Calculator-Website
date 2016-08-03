8d5f94bd24bb2376e25f1e92808094422195ff1f
anwatts <alissamwatts@gmail.com>
2016-08-03 12:49:27 -0400

    Initial add

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+/*
+function calcResults() {
+	var pBottle = document.forms["form1"]["calcPurchasePlastic"].value;
+	var aCan = document.forms["form1"]["calcPurchaseCans"].value;
+	var gBottle = document.forms["form1"]["calcPurchaseGlass"].value;
+	var lbBeef = document.forms["form1"]["calcPurchaseBeef"].value;
+	var lbPork = document.forms["form1"]["calcPurchasePork"].value;
+	var lbOtherMeat = document.forms["form1"]["calcPurchaseOtherMeat"].value;
+	var recyPBottle = document.forms["form1"]["calcRecyPlastic"].value;
+	var recyACans = document.forms["form1"]["calcRecyCans"].value;
+	var recyGBottle = document.forms["form1"]["calcRecyGlass"].value;
+	var recyBoxes = document.forms["form1"]["calcRecyBoxes"].value;
+	var recyPapers = document.forms["form1"]["calcRecyPapers"].value;
+	var recyElect = document.forms["form1"]["calcRecyElect"].value;
+	
+	var totalProduced = pBottle*2.3 + aCan*0.28 + gBottle*0.38 + lbBeef*8.0 + lbPork*6.0 +
+	lbOtherMeat*2.0;
+	var totalSaved = recyPBottle*0.15 + recyACans*0.28 + recyGBottle*0.38 + recyBoxes*0.04 + recyPapers*0.11 + recyElect*28;
+	var totalDiff = totalProduced - totalSaved;
+	
+	document.getElementById("calcResults").innerHTML = totalDiff + "lb CO2";
+	};
+	*/
